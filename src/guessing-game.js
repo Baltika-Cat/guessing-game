a0d2a858f55001cb727788422bf20aa2e445297c
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2023-10-18 02:44:31 +0300

    task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.result;
+        this.average;
+        this.max;
+        this.min;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.result = this.max - this.min;
     }
 
     guess() {
-
+        this.average = Math.round(this.result / 2);
+        return this.average;
     }
 
     lower() {
-
+        this.max = this.average;
+        this.result = this.max + this.min;
     }
 
     greater() {
-
+        this.min = this.average;
+        this.result = this.max + this.min;
     }
 }
 
